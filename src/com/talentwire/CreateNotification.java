d295c7baf44493c91af6259ee6723ca54472180c
Ordie Dordie <politicalarnold@gmail.com>
2012-04-22 23:24:00 -0700

    added a new notifications thing, need to configure

---
+++ src/com/talentwire/CreateNotification.java
@@ -0,0 +1,23 @@
+package com.talentwire;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+//import android.view.View;
+
+public class CreateNotification extends Activity {
+	
+/** Called when the activity is first created. */
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		//createNotification();
+	}
+
+	
+}
